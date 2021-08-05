d70bd59e9079287ed5700801fa04d82703f51dc9
Rohan Patel <rohanpatel4846@gmail.com>
2021-08-05 08:41:59 +0530

    Alpha 1.0

---
+++ Twitch Bulk Downloads/Twitch Bulk Downloads/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+using System.Windows.Forms;
+
+namespace Twitch_Bulk_Downloads
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 50);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(731, 247);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(731, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Clip Links With , As Separator";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(208, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 337);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select Destination Folder";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(309, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 20);
+            this.label3.TabIndex = 5;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(27, 455);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(147, 24);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Headless Chrome";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(180, 455);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(168, 24);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Rename Files (Index)";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(645, 512);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "github.com/gamble4846";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 533);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Twitch Bulk Download";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Label label2;
+        private Label label3;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Label label4;
+    }
+}
+
---
+++ Twitch Bulk Downloads/Twitch Bulk Downloads/Form1.cs
@@ -0,0 +1,119 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Twitch_Bulk_Downloads
+{
+    public partial class Form1 : Form
+    {
+        string folderPath = "";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(folderPath == "")
+            {
+                MessageBox.Show("Select Destination Folder!!!");
+            }
+            else
+            {
+                string links = richTextBox1.Text;
+                if(links == "")
+                {
+                    MessageBox.Show("Enter Links");
+                }
+                else
+                {
+                    string[] links_divided = links.Split(",");
+                    for (int i = 0; i < links_divided.Length; i++)
+                    {
+                        downloadfile(links_divided[i], folderPath, i + 1);
+                    }
+                    MessageBox.Show("Downloads Completed!!");
+                }
+            }
+        }
+
+        private void downloadfile(string Link, string SavePath, int index)
+        {
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddUserProfilePreference("download.default_directory", SavePath);
+            chromeOptions.AddUserProfilePreference("intl.accept_languages", "nl");
+            chromeOptions.AddUserProfilePreference("disable-popup-blocking", "true");
+
+            if(checkBox1.Checked)
+                chromeOptions.AddArguments("--headless");
+
+            var driverService = ChromeDriverService.CreateDefaultService();
+            driverService.HideCommandPromptWindow = true;
+
+            IWebDriver driver = new ChromeDriver(driverService, chromeOptions);
+            driver.Navigate().GoToUrl("https://clipr.xyz/");
+            IWebElement ele = driver.FindElement(By.Id("clip_url"));
+            ele.SendKeys(Link);
+            IWebElement ele1 = driver.FindElement(By.XPath("/html/body/div[2]/main/div[1]/div/div/div/div/form/button"));
+            ele1.Click();
+            
+
+            bool FoundDownloadLink = true;
+            IWebElement ele2 = null;
+            while (FoundDownloadLink)
+            {
+                try
+                {
+                    ele2 = driver.FindElement(By.XPath("/html/body/div[2]/main/div[1]/div/div/div/div/div[2]/div[2]/div[1]/a[2]"));
+                    FoundDownloadLink = false;
+                }
+                catch
+                {
+                    Thread.Sleep(25);
+                }
+            }
+            string FileName = ele2.GetAttribute("href");
+            string[] FileNames = FileName.Split("/");
+            FileName = FileNames[FileNames.Length - 1];
+            FileName = FileName.Replace("%7C", "_");
+            ele2.Click();
+
+            bool DownloadComplete = true;
+            while (DownloadComplete)
+            {
+                if (File.Exists(SavePath + "\\" + FileName))
+                    DownloadComplete = false;
+                else
+                    Thread.Sleep(25);
+            }
+
+            if(checkBox2.Checked)
+                File.Move(SavePath + "\\" + FileName, SavePath + "\\" + index + ".mp4");
+
+            driver.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+            }
+
+            label3.Text = folderPath;
+        }
+    }
+}
